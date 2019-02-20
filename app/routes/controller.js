2a0c7c23e62f14b7398fa3cff2de93b873120eff
Jefferson Rodrigues <jefferson.rodrigues@MacBook-Pro-de-Jefferson-2.local>
2019-02-20 00:35:40 -0300

    Melhorias no Controller

--- app/routes/controller.js
+++ app/routes/controller.js
@@ -2,8 +2,16 @@ module.exports = function(app){
     var json = {
         title : "Hello World com PM2!",
         projectName: "Node JS com PM2"
-    }
+    };
+    
     app.get("/home", function(req, res){
         res.send(json);
+    });
+
+    app.get("/home/:id", function(req, res){
+        res.send({
+            param : req.params,
+            queryParam: req.query
+        });
     });
 }
