74b3ec782fb960da91bbca5b34628103f6caa8d8
Jefferson Rodrigues <jefferson.rodrigues@MacBook-Pro-de-Jefferson-2.local>
2019-02-20 00:22:02 -0300

    Alterando numero de instancias

--- app/routes/controller.js
+++ app/routes/controller.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
     var json = {
-        title : "Hello World com PM2!"
+        title : "Hello World com PM2!",
+        projectName: "Node JS com PM2"
     }
     app.get("/home", function(req, res){
         res.send(json);
